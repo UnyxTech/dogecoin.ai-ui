c59381c664f1b3c8aef74810fe48ef68c4b58dd1
daniel <pangpangcoder@gmail.com>
2025-01-14 23:07:48 +0800

    add:waterfall

--- src/components/TokenDetail/TokenMarketInfo/tokenInfoTabs/PostContent.tsx
+++ src/components/TokenDetail/TokenMarketInfo/tokenInfoTabs/PostContent.tsx
@@ -1,40 +1,57 @@
-import { useVirtualizer } from "@tanstack/react-virtual";
-
+import React, { useState } from "react";
+import { Masonry } from "masonic";
+import cats from "./cats";
+import { Heart } from "lucide-react";
+// 模拟数据
+const generateItems = (count: number) => {
+  return Array.from({ length: count }, (_, i) => ({
+    id: i,
+    name: `Cat ${i}`,
+    src: randomChoice(cats),
+  }));
+};
+// @ts-expect-error test
+const randomChoice = (items) => items[Math.floor(Math.random() * items.length)];
 const PostContent = () => {
-  const items = Array.from({ length: 10000 }, (_, i) => `Item ${i}`);
-
-  const virtualizer = useVirtualizer({
-    count: items.length,
-    getScrollElement: () => document.querySelector("#scroll-container"),
-    estimateSize: () => 50,
-    overscan: 5,
-  });
+  const [items] = useState(() => generateItems(50));
 
   return (
-    <div id="scroll-container" style={{ height: "400px", overflow: "auto" }}>
-      <div
-        style={{
-          height: `${virtualizer.getTotalSize()}px`,
-          width: "100%",
-          position: "relative",
-        }}
-      >
-        {virtualizer.getVirtualItems().map((virtualItem) => (
-          <div
-            key={virtualItem.key}
-            style={{
-              position: "absolute",
-              top: 0,
-              transform: `translateY(${virtualItem.start}px)`,
-              height: `${virtualItem.size}px`,
-            }}
-          >
-            {items[virtualItem.index]}
-          </div>
-        ))}
+    <main className="min-h-screen w-full">
+      <div className="p-2 w-full max-w-4xl mx-auto">
+        <Masonry
+          items={items}
+          columnGutter={8}
+          columnWidth={172}
+          overscanBy={5}
+          render={FakeCard}
+        />
       </div>
-    </div>
+    </main>
   );
 };
+
+interface CardProps {
+  data: {
+    id: number;
+    name: string;
+    src: string;
+  };
+}
+
+const FakeCard = ({ data: { src } }: CardProps) => (
+  <div className="group mb-2 bg-gray-800  w-full min-h-[100px] transition-transform duration-200 hover:shadow-lg ">
+    <img className="w-full block rounded-t-sm" alt="kitty" src={src} />
+    <div className="flex items-center justify-between p-2">
+      <div className="flex items-center gap-2 ">
+        <img src="/images/icon_doge.svg" alt="" />
+        <span>xxxxx</span>
+      </div>
+      <div className="flex items-center">
+        <Heart size={16} />
+        <span>13</span>
+      </div>
+    </div>
+  </div>
+);
 
 export default PostContent;
