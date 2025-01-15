3718c865c34ab3ea289c2f38718b19b64aa0028b
daniel <pangpangcoder@gmail.com>
2025-01-15 11:06:31 +0800

    add:tradingView

--- src/components/TokenDetail/TokenMarketInfo/tradingview/index.tsx
+++ src/components/TokenDetail/TokenMarketInfo/tradingview/index.tsx
@@ -1,174 +1,105 @@
-import React, { useEffect, useRef } from "react";
-import { ColorType, createChart, IChartApi } from "lightweight-charts";
+import { useEffect, useRef } from "react";
+import {
+  widget,
+  ChartingLibraryWidgetOptions,
+  LanguageCode,
+  ResolutionString,
+} from "../../../../../public/charting_library";
+import * as React from "react";
+
+export interface ChartContainerProps {
+  symbol: ChartingLibraryWidgetOptions["symbol"];
+  interval: ChartingLibraryWidgetOptions["interval"];
+
+  // BEWARE: no trailing slash is expected in feed URL
+  datafeedUrl: string;
+  libraryPath: ChartingLibraryWidgetOptions["library_path"];
+  chartsStorageUrl: ChartingLibraryWidgetOptions["charts_storage_url"];
+  chartsStorageApiVersion: ChartingLibraryWidgetOptions["charts_storage_api_version"];
+  clientId: ChartingLibraryWidgetOptions["client_id"];
+  userId: ChartingLibraryWidgetOptions["user_id"];
+  fullscreen: ChartingLibraryWidgetOptions["fullscreen"];
+  autosize: ChartingLibraryWidgetOptions["autosize"];
+  studiesOverrides: ChartingLibraryWidgetOptions["studies_overrides"];
+  container: ChartingLibraryWidgetOptions["container"];
+}
+
+const getLanguageFromURL = (): LanguageCode | null => {
+  const regex = new RegExp("[\\?&]lang=([^&#]*)");
+  const results = regex.exec(location.search);
+  return results === null
+    ? null
+    : (decodeURIComponent(results[1].replace(/\+/g, " ")) as LanguageCode);
+};
 
 const TradingViewChart = () => {
-  const chartContainerRef = useRef<HTMLDivElement>(null);
-  const chartRef = useRef<IChartApi | null>(null);
-  const seriesRef = useRef(null);
-  const intervalIdRef = useRef();
-  const samplePoint = (i: number) => {
-    const randomFactor = 25 + Math.random() * 25;
-    return (
-      i *
-        (0.5 +
-          Math.sin(i / 1) * 0.2 +
-          Math.sin(i / 2) * 0.4 +
-          Math.sin(i / randomFactor) * 0.8 +
-          Math.sin(i / 50) * 0.5) +
-      200 +
-      i * 2
-    );
-  };
+  const chartContainerRef =
+    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
 
-  const generateData = (
-    numberOfCandles = 500,
-    updatesPerCandle = 5,
-    startAt = 100
-  ) => {
-    const createCandle = (val: number, time: number) => ({
-      time,
-      open: val,
-      high: val,
-      low: val,
-      close: val,
-    });
-
-    const updateCandle = (candle: any, val: number) => ({
-      time: candle.time,
-      close: val,
-      open: candle.open,
-      low: Math.min(candle.low, val),
-      high: Math.max(candle.high, val),
-    });
-
-    const date = new Date(Date.UTC(2018, 0, 1, 12, 0, 0, 0));
-    const numberOfPoints = numberOfCandles * updatesPerCandle;
-    const initialData = [];
-    const realtimeUpdates = [];
-    let lastCandle;
-    let previousValue = samplePoint(-1);
-
-    for (let i = 0; i < numberOfPoints; ++i) {
-      if (i % updatesPerCandle === 0) {
-        date.setUTCDate(date.getUTCDate() + 1);
-      }
-      const time = date.getTime() / 1000;
-      let value = samplePoint(i);
-      const diff = (value - previousValue) * Math.random();
-      value = previousValue + diff;
-      previousValue = value;
-
-      if (i % updatesPerCandle === 0) {
-        const candle = createCandle(value, time);
-        lastCandle = candle;
-        if (i >= startAt) {
-          realtimeUpdates.push(candle);
-        }
-      } else {
-        const newCandle = updateCandle(lastCandle, value);
-        lastCandle = newCandle;
-        if (i >= startAt) {
-          realtimeUpdates.push(newCandle);
-        } else if ((i + 1) % updatesPerCandle === 0) {
-          initialData.push(newCandle);
-        }
-      }
-    }
-
-    return {
-      initialData,
-      realtimeUpdates,
-    };
+  const defaultProps: Omit<ChartContainerProps, "container"> = {
+    symbol: "AAPL",
+    interval: "D" as ResolutionString,
+    datafeedUrl: "https://demo_feed.tradingview.com",
+    libraryPath: "/charting_library/",
+    chartsStorageUrl: "https://saveload.tradingview.com",
+    chartsStorageApiVersion: "1.1",
+    clientId: "tradingview.com",
+    userId: "public_user_id",
+    fullscreen: false,
+    autosize: true,
+    studiesOverrides: {},
   };
 
   useEffect(() => {
-    if (!chartContainerRef.current) return;
+    const widgetOptions: ChartingLibraryWidgetOptions = {
+      symbol: defaultProps.symbol as string,
+      // BEWARE: no trailing slash is expected in feed URL
+      // tslint:disable-next-line:no-any
+      datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(
+        defaultProps.datafeedUrl
+      ),
+      interval:
+        defaultProps.interval as ChartingLibraryWidgetOptions["interval"],
+      container: chartContainerRef.current,
+      library_path: defaultProps.libraryPath as string,
 
-    chartRef.current = createChart(chartContainerRef.current, {
-      layout: {
-        textColor: "black",
-        background: { type: ColorType.Solid, color: "white" },
-      },
-      height: 400,
-      width: chartContainerRef.current.clientWidth,
-    });
-    //@ts-expect-error text
-    seriesRef.current = chartRef.current.addCandlestickSeries({
-      upColor: "#26a69a",
-      downColor: "#ef5350",
-      borderVisible: false,
-      wickUpColor: "#26a69a",
-      wickDownColor: "#ef5350",
-    });
-
-    const data = generateData(2500, 20, 1000);
-    //@ts-expect-error text
-    seriesRef.current.setData(data.initialData);
-
-    chartRef.current.timeScale().fitContent();
-    //@ts-expect-error text
-    chartRef.current.timeScale().scrollToPosition(5);
-
-    // //@ts-expect-error text
-    function* getNextRealtimeUpdate(realtimeData: any[]) {
-      for (const dataPoint of realtimeData) {
-        yield dataPoint;
-      }
-      return null;
-    }
-
-    const streamingDataProvider = getNextRealtimeUpdate(data.realtimeUpdates);
-    //@ts-expect-error text
-    intervalIdRef.current = setInterval(() => {
-      const update = streamingDataProvider.next();
-      if (update.done) {
-        clearInterval(intervalIdRef.current);
-        return;
-      }
-      //@ts-expect-error text
-      seriesRef.current?.update(update.value);
-    }, 1000);
-
-    // 响应窗口大小变化
-    const handleResize = () => {
-      if (chartRef.current && chartContainerRef.current) {
-        chartRef.current.applyOptions({
-          width: chartContainerRef.current.clientWidth,
-        });
-      }
+      locale: getLanguageFromURL() || "en",
+      disabled_features: ["use_localstorage_for_settings"],
+      enabled_features: ["study_templates"],
+      charts_storage_url: defaultProps.chartsStorageUrl,
+      charts_storage_api_version: defaultProps.chartsStorageApiVersion,
+      client_id: defaultProps.clientId,
+      user_id: defaultProps.userId,
+      fullscreen: defaultProps.fullscreen,
+      autosize: defaultProps.autosize,
+      studies_overrides: defaultProps.studiesOverrides,
     };
 
-    window.addEventListener("resize", handleResize);
+    const tvWidget = new widget(widgetOptions);
 
-    // 清理函数
+    tvWidget.onChartReady(() => {
+      tvWidget.headerReady().then(() => {
+        const button = tvWidget.createButton();
+        button.setAttribute("title", "Click to show a notification popup");
+        button.classList.add("apply-common-tooltip");
+        button.addEventListener("click", () =>
+          tvWidget.showNoticeDialog({
+            title: "Notification",
+            body: "TradingView Charting Library API works correctly",
+            callback: () => {
+              console.log("Noticed!");
+            },
+          })
+        );
+        button.innerHTML = "Check API";
+      });
+    });
+
     return () => {
-      window.removeEventListener("resize", handleResize);
-      if (intervalIdRef.current) {
-        clearInterval(intervalIdRef.current);
-      }
-      if (chartRef.current) {
-        chartRef.current.remove();
-      }
+      tvWidget.remove();
     };
-  }, []);
+  });
 
-  const handleGoToRealtime = () => {
-    chartRef.current?.timeScale().scrollToRealTime();
-  };
-
-  return (
-    <div className="w-full">
-      <div ref={chartContainerRef} />
-      <div className="flex gap-2 mt-4">
-        <button
-          className="px-6 py-2 bg-[#F0F3FA] hover:bg-[#E0E3EB] active:bg-[#D1D4DC] rounded-lg text-[#131722] font-medium"
-          onClick={handleGoToRealtime}
-        >
-          Go to realtime
-        </button>
-      </div>
-    </div>
-  );
+  return <div ref={chartContainerRef} className="h-full" />;
 };
-
 export default TradingViewChart;
